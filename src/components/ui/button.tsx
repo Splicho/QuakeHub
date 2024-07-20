bf105c22058b35d32bb8229a2c0e5ab892bbec20
Splicho <d.cacian@gmx.de>
2024-07-20 21:42:26 +0200

    Added drawer and adjusted server list

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -47,7 +47,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
-        {...props}
+        {...props} // Ensure onClick and other props are passed down
       />
     )
   }
